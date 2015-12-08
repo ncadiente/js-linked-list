8394d430e4c8b30cf47163fd4e28b009154ce19b
Nick Cadiente <nickcadiente@Nicks-MacBook-Pro.local>
2015-12-08 12:28:06 -1000

    passes all test using Joe's code as a reference

--- linkedList.js
+++ linkedList.js
@@ -3,42 +3,19 @@
  * @description  Main Module
  * @return {Object} an object exposing methods to be used to manipulate a linked list
  */
- var example = {
-  value: 'Ready Player One',
-  next: {
-    value: '1982',
-    next: {
-      value: 'Neuromancer',
-      next: {
-        value: 'Snow Crash',
-        next: null
-      }
-    }
-  }
-};
 
 function linkedListGenerator(){
   var module = {};
-  var linkedList = {};
-  var node = {
-    value : null,
-    next : null
-  };
   var head = null;
   var tail = null;
-  var position = 0;
+  var length = 0;
 
-  function traverse(list){
-    if (list.next === undefined) {
-      return null;
-    }
-    if (list.next === null){
-      return this;
-    }
-    position++;
-    traverse(this.next);
+  function makeNode(value){
+    return {
+    value : value,
+    next : null
+    };
   }
-
 
   module.getHead = function() {
       return head;
@@ -48,76 +25,70 @@ function linkedListGenerator(){
     return tail;
   };
 
-  module.add = function(newValue) {
-    var newNode = {
-      value : newValue,
-      next : null
-    };
-    if (head === null && tail === null){
+  module.add = function(value) {
+    var newNode = makeNode(value);
+    if (tail === null){
       head = newNode;
-      tail = newNode;
-      return tail;
     } else {
-    tail.next = newNode;
-    tail = newNode;
-    return tail;
+      tail.next = newNode;
     }
+    tail = newNode;
+    length++;
+    return newNode;
   };
 
   module.get = function(index) {
-    var got = this.getHead();
+    var currentNode = this.getHead();
     var position = 0;
     while (position < index){
-      position ++;
-      if (got.next === null) {
+      if (currentNode === tail) {
         return false;
       }
-      got = got.next;
+      currentNode = currentNode.next;
+      position ++;
     }
-    return got;
-  };
+    return currentNode;
+   };
 
   module.remove = function(index) {
-    var toRemove = this.get(index);
-    if (toRemove === false){
+    var currentNode = this.get(index);
+    var previousNode = this.get(index - 1);
+    if ( index >= length ) {
       return false;
     }
-    if (toRemove === head){
-      head = toRemove.next;
-      return true;
-    }
-    if (toRemove === tail){
-      tail = this.get(index - 1);
-      tail.next = null;
-      return true;
-    }
-    if (toRemove !== head && toRemove !== tail){
-      this.get(index - 1).next = this.get(index + 1);
-    }
-
-
-  };
-
-
-  module.insert = function(value, index) {
-    var toShift = this.get(index);
-    node.value = value;
-    node.next = toShift;
-
-    if (this.get(index) === false) {
-      return false;
-    }
-    if (index === 0){
-      head = node;
-      return true;
+    if (length === 1 && index === 0) {
+      head = null;
+      tail = null;
     } else {
-    console.log(this.getHead());
-    this.get(index-1).next = node;
-    return true;
+      if (index === 0) {
+        head = currentNode.next;
+      } else if (index === length - 1){
+        tail = previousNode;
+        previousNode.next = null;
+      } else {
+        previousNode.next = currentNode.next;
+      }
     }
+    length--;
   };
+
+
+   module.insert = function(value, index) {
+    var currentNode = this.get(index);
+    var previousNode = this.get(index - 1);
+    var tempNode = makeNode(value);
+
+    if ( index >= length || index <0) {
+      return false;
+    } else if ( index === 0){
+      tempNode.next = currentNode;
+      head = tempNode;
+    } else {
+    previousNode.next = tempNode;
+    tempNode.next = currentNode;
+    }
+    length++;
+   };
 
   return module;
 }
-
-linkedListGenerator().get(0);
