586ba0262d04b34d939df30be6a094ae1cef994a
Nick Cadiente <nickcadiente@Nicks-MacBook-Pro.local>
2015-12-07 15:25:40 -1000

    passes some add tests

--- linkedList.js
+++ linkedList.js
@@ -3,6 +3,79 @@
  * @description  Main Module
  * @return {Object} an object exposing methods to be used to manipulate a linked list
  */
-function linkedListGenerator(){
+ var example = {
+  value: 'Ready Player One',
+  next: {
+    value: '1982',
+    next: {
+      value: 'Neuromancer',
+      next: {
+        value: 'Snow Crash',
+        next: null
+      }
+    }
+  }
+};
 
+function linkedListGenerator(){
+  var module = {};
+  var linkedList = {};
+  var node = {
+    value : null,
+    next : null
+  };
+  var head;
+  var tail;
+
+
+  module.getHead = function() {
+    if (this.value === undefined){
+      return null;
+    }
+      return this;
+    };
+
+  module.getTail = function() {
+    if (this.value === undefined){
+      return null;
+    }
+    if (this.next === null){
+      return this;
+    }else{
+      this.next.getTail();
+    }
+  };
+
+  module.add = function(newValue) {
+    if (this.value === undefined){
+      this.value = newValue;
+      this.next = null;
+      return this;
+    }
+    if (this.next === null){
+      var newNode = node;
+      newNode.value = newValue;
+      newNode.next = null;
+      this.next = newNode;
+      return newNode;
+    }else{
+      this.next.add();
+    }
+  };
+
+  module.remove = function() {
+
+  };
+
+  module.get = function() {
+
+  };
+
+  module.insert = function() {
+
+  };
+
+  return module;
 }
+
+console.log(example.linkedListGenerator.getTail);
